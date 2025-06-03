bab44e4865120c63945c4fa22322d4614762632e
Maarten Wijnants <wijnants.maarten@gmail.com>
2025-06-03 15:03:00 +0200

    Update RunInstallomator.sh

--- RunInstallomator.sh
+++ RunInstallomator.sh
@@ -4,10 +4,13 @@ MyPath=$1
 InstallomatorPath=$2
 
 # Get a list of labels and run them all in Installomator.
-fileItemString=$(cat $MyPath/Labels.txt |tr "\n" " ")
+fileItemString=$(cat "$MyPath/Labels.txt" | tr "\n" " ")
 labels=($fileItemString)
 
 for label in "${labels[@]}"
 do
-    $InstallomatorPath/Installomator.sh $label NOTIFY=success DEBUG=0
+    # Only run if label doesn't start with #
+    if [[ ! $label =~ ^# ]]; then
+        "$InstallomatorPath/Installomator.sh" "$label" NOTIFY=success DEBUG=0
+    fi
 done
